da9c33eccdfc237c0226615d26a1c163cb535db1
Yampier Medina <jpmti2016@gmail.com>
2022-01-15 05:41:31 -0500

    Create basic theme and navigation

--- components/Nav.js
+++ components/Nav.js
@@ -4,7 +4,7 @@ import { useTheme } from "next-themes";
 import { MoonIcon, SunIcon } from "@heroicons/react/solid";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import aerxLogo from "../public/images/ae_mini.jpg";
+import aerxLogo from "../public/images/aerx_logo-removebg-preview 1.svg";
 import Menu from "./Menu";
 import MobileButton from "./MobileButton";
 import ChangeLanguage from "./ChangeLanguage";
@@ -20,50 +20,26 @@ export default function Nav() {
   if (!mounted) return null;
 
   return (
-    <nav
-      className="sticky top-0 z-50 w-full px-4 sm:items-center sm:flex sm:flex-row sm:py-4 md:px-24"
-      style={{ backgroundColor: "#0F0111" }}
-    >
+    <nav className="sticky top-0 z-50 items-center w-full px-4 bg-transparent sm:items-center sm:flex sm:flex-row sm:py-4 md:px-20">
       <div className="flex flex-row items-center justify-center w-full p-2 sm:w-auto">
-        <Link href={{ pathname: "/", hash: "about" }}>
-          <a
-            className={`${
-              asPath === "/#about"
-                ? "nav-item underline rounded-full p-0 m-0"
-                : "nav-item no-underline rounded-full p-0 m-0"
-            }`}
-          >
-            <div className="w-8 h-8 border-2 border-gray-900 rounded-full dark:border-gray-100 sm:h-10 sm:w-10 lg:h-16 lg:w-16">
-              <Image
-                src={aerxLogo}
-                className="rounded-full"
-                layout="responsive"
-                alt="aerx logo"
-                priority
-                height={100}
-                width={100}
-              />
-            </div>
+        <Link href={{ pathname: "/", hash: "welcome" }}>
+          <a className="w-[78px] h-[75px] bg-transparent hover:bg-white">
+            <Image
+              src={aerxLogo}
+              className="rounded-sm"
+              layout="responsive"
+              alt="aerx logo"
+              priority
+              height={100}
+              width={100}
+            />
           </a>
         </Link>
 
-        <button
-          aria-label="toggle dark mode"
-          className="ml-4 font-semibold rounded-full sm:ml-8 dark:text-yellow-600 dark:hover:text-yellow-400 dark:bg-gray-900 hover:text-gray-600"
-          onClick={() => {
-            setTheme(theme === "light" ? "dark" : "light");
-          }}
-        >
-          {theme === "dark" ? (
-            <SunIcon className="w-5 h-5 sm:h-8 sm:w-8" />
-          ) : (
-            <MoonIcon className="w-5 h-5 sm:h-8 sm:w-8" />
-          )}
-        </button>
-        <div className="block sm:hidden">
-          <ChangeLanguage />
-        </div>
         <MobileButton setToggled={setToggled} toggled={toggled} />
+      </div>
+      <div className="block sm:hidden">
+        <ChangeLanguage />
       </div>
       <Menu setToggled={setToggled} toggled={toggled} />
     </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,23 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#9510AC",
+          dark: "#0F0111",
+          gray: "#202020",
+          text: "#747474",
+          social: "#2D76F9",
+        },
+        team: {
+          header: "#252B42",
+          content: "#737373",
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/aspect-ratio")],
 };
