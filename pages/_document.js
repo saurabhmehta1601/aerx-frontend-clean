0385b0dd9e5607c2237a8ffce0427c6104f78ca8
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-01-28 15:07:19 +0530

    feat(cardoutlined.js): add cardOutlined component

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,15 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>{/* Fonts will be here */}</Head>
+        <Head>
+          {/* Fonts will be here */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Jost&family=Work+Sans&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body className="text-white bg-brand-dark">
           <Main />
           <NextScript />
