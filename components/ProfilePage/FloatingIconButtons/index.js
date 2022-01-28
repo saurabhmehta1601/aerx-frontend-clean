de33d227a8ff99b6b426c0e6531ba809024d7f89
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-01-28 15:10:19 +0530

    feat(userinfo.js): add userInfo component

--- components/ProfilePage/FloatingIconButtons/index.js
+++ components/ProfilePage/FloatingIconButtons/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import IconButtonsGroup from "../../UI/IconButtonsGroup";
 import IconButton from "../../UI/IconButton";
-import { FcSearch, FcPlus } from "react-icons/fc";
+import { FcSearch } from "react-icons/fc";
 import { FaPlus } from "react-icons/fa";
 
 const FloatingIconButtons = () => {
   return (
-    <IconButtonsGroup>
+    <IconButtonsGroup orientation="vertical">
       <IconButton>
         <FcSearch />
       </IconButton>
--- components/UI/IconButtonsGroup/index.js
+++ components/UI/IconButtonsGroup/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import styles from "./styles.module.css";
 
 const IconButtonsGroups = (props) => {
-  return <div className={styles.iconButtonsGroup}>{props.children}</div>;
+  return (
+    <div
+      className={`${styles.iconButtonsGroup} ${
+        props.orientation === "vertical" ? styles.vertical : ""
+      } `}
+    >
+      {props.children}
+    </div>
+  );
 };
 
 export default IconButtonsGroups;
